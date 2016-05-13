c13663cc8ea37273cd439e221c59949f293dcbd9
FranFuentesA <fran_fuentes20@hotmail.com>
2016-05-13 12:23:26 +0200

    Comit inicial con las modificaciones realizadas

---
+++ Post/MessagePost.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class MessagePost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class MessagePost extends Post
+{
+    private String message;  
+
+    /**
+     * Constructor for objects of class MessagePost
+     */
+    public MessagePost(String author, String text)
+    {
+       super(author);     
+       this.message = text;            
+    }      
+    
+    /**
+     * Metodo que devuelve el mensaje del post
+     */
+    public String getText(){
+        return message;
+    }      
+    
+}
+
---
+++ Post/PhotoPost.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class MessagePost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PhotoPost extends Post
+{
+    private String filename;
+    private String caption;
+
+    /**
+     * Constructor for objects of class MessagePost
+     */
+    public PhotoPost(String author, String filename, String caption)
+    {
+       super(author);  
+       this.filename = filename;
+       this.caption = caption;      
+    }   
+    
+    /**
+     * Meotdo que devuelve el nombre del archivo
+     */
+    public String getImageFile(){
+        return filename;
+    }
+    
+    /**
+     * Metodo que devuelve el mensaje del post
+     */
+    public String getCaption(){
+        return caption;
+    }    
+   
+}
+    
